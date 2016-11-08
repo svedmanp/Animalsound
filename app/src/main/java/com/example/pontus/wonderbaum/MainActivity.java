c9d73604560251ae56405a987e7369c1f87bebce
Svedmanp <gussvedpo@student.gu.se>
2016-11-08 12:50:39 +0100

    HALLO HANNAH enna

---
+++ app/src/main/java/com/example/pontus/wonderbaum/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pontus.wonderbaum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//pruttidapoopi
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
